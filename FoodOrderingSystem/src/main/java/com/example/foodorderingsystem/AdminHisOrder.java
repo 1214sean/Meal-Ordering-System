8fa43b1bfd096acff6d3305944dce1f2ed9c92b9
1214sean <seanhuang922922@gmail.com>
2022-02-13 22:13:15 +0800

    2/13

--- FoodOrderingSystem/src/main/java/com/example/foodorderingsystem/AdminHisOrder.java
+++ FoodOrderingSystem/src/main/java/com/example/foodorderingsystem/AdminHisOrder.java
@@ -6,10 +6,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
-import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 import java.io.*;
@@ -18,37 +16,9 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
 
-public class ReserveHis {
+public class AdminHisOrder {
 
     private static final String COMMA_DELIMITER = ",";
-
-
-    @FXML
-    private Label RO_Date01;
-
-    @FXML
-    private Label RO_Date02;
-
-    @FXML
-    private Label RO_Date03;
-
-    @FXML
-    private ImageView RO_Image01;
-
-    @FXML
-    private ImageView RO_Image02;
-
-    @FXML
-    private ImageView RO_Image03;
-
-    @FXML
-    private Label RO_Item01;
-
-    @FXML
-    private Label RO_Item02;
-
-    @FXML
-    private Label RO_Item03;
 
     @FXML
     private Button futureok;
@@ -63,127 +33,13 @@ public class ReserveHis {
     private MenuItem ok3;
 
     @FXML
-    private MenuItem ok4;
-
-    @FXML
     private Button pastok;
 
     @FXML
     private MenuButton viewfuture;
 
-
-
     @FXML
     private MenuButton viewpast;
-
-
-
-    @FXML
-    int orderClicked(ActionEvent event, int i) {
-        System.out.println(i);
-        write_i(i);
-
-
-        return i;
-    }
-
-    @FXML
-    protected void initialize() {
-        System.out.println("RESERVEHISSS!!!!");
-        //read();
-        //List<List<String>> a=read();
-        //write(a);
-        //changeviewfuture1(a);        //第一種改menuiten name 的方式
-        List<List<String>> reserve=read();
-        Collections.sort(reserve, new CustomComparator());
-
-        viewfuture.getItems().clear();
-        LocalDate date = LocalDate.now();
-        DateTimeFormatter formatter_date = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        System.out.println(date.format(formatter_date));
-        String comparedate= date.format(formatter_date);
-
-        LocalTime time = LocalTime.now();
-        DateTimeFormatter formatter_time = DateTimeFormatter.ofPattern("HH:mm:ss");
-        System.out.println(time.format(formatter_time));
-        String comparetime=time.format(formatter_time);   //判斷訂單是future or past
-
-        int i;
-
-        for( i=0; i<reserve.size();i++) {
-            if (
-                    (comparedate.compareTo(reserve.get(i).get(0)) < 0) ||
-                            (comparedate.compareTo(reserve.get(i).get(0)) == 0 && comparetime.compareTo("12:30:00") < 0)
-            ) {
-                break;
-            }
-
-        }
-
-
-
-
-        for( int j=0; j<i;j++)
-        {
-            String c=reserve.get(j).get(0)+reserve.get(j).get(1)+reserve.get(j).get(2);
-            MenuItem item=new MenuItem(c);
-            viewpast.getItems().add(item);
-            final int j_final_past =j;
-            item.setOnAction(event -> orderClicked(event, j_final_past)); //onAction pragmtically by using anomynomus  fuciton when click the certain vie future order, it atomaticlly writes into the gile of which intend to open
-
-
-        }
-        for( int j=i; j<reserve.size();j++)
-        {
-            String c=reserve.get(j).get(0)+reserve.get(j).get(1)+reserve.get(j).get(2);
-            MenuItem item=new MenuItem(c);
-            viewfuture.getItems().add(item);
-            final int j_final_future =j;
-            item.setOnAction(event -> orderClicked(event, j_final_future));
-
-        }
-
-
-
-
-
-        //自動生成menuitem + 更新menu item name
-
-
-
-        List<List<String>> a=read_studentOrder();
-        if (a.size()==0) {
-
-        }else if (a.size()==1){
-            RO_Item01.setText(String.valueOf(a.get(0).get(2)));
-            RO_Date01.setText(String.valueOf(a.get(0).get(0)));
-
-        }else if (a.size()==2){
-                RO_Item01.setText(String.valueOf(a.get(0).get(2)));
-                RO_Date01.setText(String.valueOf(a.get(0).get(0)));
-                RO_Item02.setText(String.valueOf(a.get(1).get(2)));
-                RO_Date02.setText(String.valueOf(a.get(1).get(0)));
-            }else{
-                RO_Item01.setText(String.valueOf(a.get(a.size()-1).get(2)));
-                RO_Date01.setText(String.valueOf(a.get(a.size()-1).get(0)));
-                RO_Item02.setText(String.valueOf(a.get(a.size()-2).get(2)));
-                RO_Date02.setText(String.valueOf(a.get(a.size()-2).get(0)));
-                RO_Item03.setText(String.valueOf(a.get(a.size()-3).get(2)));
-                RO_Date03.setText(String.valueOf(a.get(a.size()-3).get(0)));
-
-            }
-
-
-
-
-
-
-
-
-
-    }
-
-
 
     @FXML
     void FutureOk(ActionEvent event) throws IOException {
@@ -210,10 +66,75 @@ public class ReserveHis {
     }
 
     @FXML
+    int orderClicked(ActionEvent event, int i) {
+        System.out.println(i);
+        write_i(i);
+
+
+        return i;
+    }
+
+
+    @FXML
+    protected void initialize() {
+        System.out.println("RESERVEHISSS!!!!");
+        //read();
+        //List<List<String>> a=read();
+        //write(a);
+        //changeviewfuture1(a);        //第一種改menuiten name 的方式
+        List<List<String>> reserve = read();
+        Collections.sort(reserve, new CustomComparator_Admin());
+
+        viewfuture.getItems().clear();
+        LocalDate date = LocalDate.now();
+        DateTimeFormatter formatter_date = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        System.out.println(date.format(formatter_date));
+        String comparedate = date.format(formatter_date);
+
+        LocalTime time = LocalTime.now();
+        DateTimeFormatter formatter_time = DateTimeFormatter.ofPattern("HH:mm:ss");
+        System.out.println(time.format(formatter_time));
+        String comparetime = time.format(formatter_time);   //判斷訂單是future or past
+
+        int i;
+
+        for (i = 0; i < reserve.size(); i++) {
+            if (
+                    (comparedate.compareTo(reserve.get(i).get(0)) < 0) ||
+                            (comparedate.compareTo(reserve.get(i).get(0)) == 0 && comparetime.compareTo("12:30:00") < 0)
+            ) {
+                break;
+            }
+
+        }
+
+
+        for (int j = 0; j < i; j++) {
+            String c = reserve.get(j).get(0) + reserve.get(j).get(1) + reserve.get(j).get(2);
+            MenuItem item = new MenuItem(c);
+            viewpast.getItems().add(item);
+            final int j_final_past = j;
+            item.setOnAction(event -> orderClicked(event, j_final_past)); //onAction pragmtically by using anomynomus  fuciton when click the certain vie future order, it atomaticlly writes into the gile of which intend to open
+
+
+        }
+        for (int j = i; j < reserve.size(); j++) {
+            String c = reserve.get(j).get(0) + reserve.get(j).get(1) + reserve.get(j).get(2);
+            MenuItem item = new MenuItem(c);
+            viewfuture.getItems().add(item);
+            final int j_final_future = j;
+            item.setOnAction(event -> orderClicked(event, j_final_future));
+
+        }
+    }
+
+
+
+    @FXML
     void Ok2(ActionEvent event) throws IOException
     {
         System.out.println("HI");
-        Parent blah = FXMLLoader.load(getClass().getResource("ReserveHis.fxml"));
+        Parent blah = FXMLLoader.load(getClass().getResource("AdminHisOrder.fxml"));
         Scene scene = new Scene(blah);
 
         Stage appStage = (Stage) futureok.getScene().getWindow();
@@ -225,26 +146,13 @@ public class ReserveHis {
     void Ok3(ActionEvent event) throws IOException
     {
         System.out.println("HI");
-        Parent blah = FXMLLoader.load(getClass().getResource("PersonInfo.fxml"));
+        Parent blah = FXMLLoader.load(getClass().getResource("AdminEdit.fxml"));
         Scene scene = new Scene(blah);
 
         Stage appStage = (Stage) futureok.getScene().getWindow();
         appStage.setScene(scene);
         appStage.show();
     }
-
-    @FXML
-    void Ok4(ActionEvent event) throws IOException
-    {
-        System.out.println("HI");
-        Parent blah = FXMLLoader.load(getClass().getResource("LobbyPage.fxml"));
-        Scene scene = new Scene(blah);
-
-        Stage appStage = (Stage) futureok.getScene().getWindow();
-        appStage.setScene(scene);
-        appStage.show();
-    }
-
 
     @FXML
     void PastOk(ActionEvent event) {
@@ -257,20 +165,9 @@ public class ReserveHis {
     }
 
     @FXML
-    void ViewFuture1(ActionEvent event) {
-
-    }
-
-    @FXML
-    void ViewFuture2(ActionEvent event) {
-
-    }
-
-    @FXML
     void ViewPast(ActionEvent event) {
 
     }
-
 
 
     private static List<List<String>> read() {
@@ -290,47 +187,6 @@ public class ReserveHis {
         System.out.println(records.toString());
         return records;
     }
-
-
-
-    /*private static void write(List<List<String>> data) {
-        String ans = "";
-        for(int i = 0; i < data.size(); i++) {
-            for(int j = 0; j < data.get(i).size(); j++) {
-                ans += String.valueOf(data.get(i).get(j));
-                if(j != data.get(i).size() - 1) ans += ",";
-            }
-            if(i != data.size() - 1) ans += "\n";
-        }
-        System.out.print(ans);
-
-        try {
-            FileWriter myWriter = new FileWriter("/Users/sean/Documents/GitHub/Meal-Ordering-System/FoodOrderingSystem/src/main/java/com/example/foodorderingsystem/writtest.csv");
-            myWriter.write(ans);
-            myWriter.close();
-            System.out.println("Successfully wrote to the file.");
-        } catch (IOException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
-
-
-    } */
-
-    /*private void changeviewfuture1(List<List<String>> data) {
-        String ans = "";
-        for(int i = 0; i < data.size(); i++) {
-            for(int j = 0; j < data.get(i).size(); j++) {
-                ans += String.valueOf(data.get(i).get(j));
-                if(j != data.get(i).size() - 1) ans += ",";
-            }
-            if(i != data.size() - 1) ans += "\n";
-            viewfuture1.setText(ans);
-        }
-
-
-
-    } */
 
     private static void write_i(int i) {
         System.out.println(i);
@@ -366,10 +222,12 @@ public class ReserveHis {
         System.out.println(records.toString());
         return records;
     }
+
+
 }
 
 
-class CustomComparator implements Comparator<List<String>> //判斷日期順序 整理資料排列從最新到最舊
+class CustomComparator_Admin implements Comparator<List<String>> //判斷日期順序 整理資料排列從最新到最舊
 {
     @Override
     public int compare(List<String> o1,
--- FoodOrderingSystem/src/main/java/com/example/foodorderingsystem/Adminfutureo.java
+++ FoodOrderingSystem/src/main/java/com/example/foodorderingsystem/Adminfutureo.java
@@ -5,70 +5,391 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ListView;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
 import javafx.stage.Stage;
 
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Adminfutureo {
 
-    @FXML
-    private ListView<?> lsview;
+    private static final String COMMA_DELIMITER = ",";
 
     @FXML
-    private MenuItem ok;
+    private Label Item1;
+
+    @FXML
+    private Label date;
+
+    @FXML
+    private Button delete_order;
+
+    @FXML
+    private MenuItem ok1;
+
+    @FXML
+    private MenuItem ok2;
 
     @FXML
     private MenuItem ok3;
 
     @FXML
-    private MenuItem ok4;
+    private Label orderstuclass;
+
+    @FXML
+    private Label orderstuname;
+
+    @FXML
+    private Label time;
+
+    @FXML
+    private Label totalprice;
+
+    @FXML
+    protected void initialize() {
+        int b=readcommu_i();
+        List<String> c=read_specificOrder(b);
+        //  id.settext(c.get(0))
+        date.setText(c.get(0)); //only 一維List of String
+        time.setText(c.get(1));
+        orderstuname.setText(c.get(3));
+        orderstuclass.setText(c.get(4));
+
+
+        List<String> d=read_specificOrderItems(b);
+        int dsize=d.size();
+        String buyitems="";
+        for(int i =0;i<dsize;i++)
+            buyitems=buyitems+" "+d.get(i);
+        Item1.setText(buyitems);
+
+        List<String> e=read_specificOrderPrice(b);
+        totalprice.setText(e.get(0)+" NTD");
+
+
+
+
+    }
+
+    @FXML
+    protected void Delete_Order() throws IOException {
+
+        List<List<String>> b = read_StudentOrder(); //讀取原本資料
+        List<List<String>> c = Deleting(b);//刪資料
+        write_StudentOrder(c);
+
+
+
+        List<List<String>> x = read_SOI(); //讀取原本資料
+        List<List<String>> y = Deleting(x);
+        write_SOI(y);
+
+
+        List<List<String>> e = read_SOP(); //讀取原本資料
+        List<List<String>> f = Deleting(e);
+        write_SOP(f);
+
+
+
+        System.out.println("HI");
+        Parent blah = FXMLLoader.load(getClass().getResource("AdminHisOrder.fxml"));
+        Scene scene = new Scene(blah);
+
+        Stage appStage = (Stage) delete_order.getScene().getWindow();
+        appStage.setScene(scene);
+        appStage.show();
+        //replacing the original order with deleted order
+
+        // NEED another line to go back to  the previous page
+    }
 
     @FXML
     void Ok1(ActionEvent event) throws IOException {
-        System.out.println("HI");
-        Parent blah = FXMLLoader.load(getClass().getResource("SignIn.fxml"));
-        Scene scene = new Scene(blah);
+        {
+            System.out.println("HI");
+            Parent blah = FXMLLoader.load(getClass().getResource("SignIn.fxml"));
+            Scene scene = new Scene(blah);
 
-        Stage appStage = (Stage) lsview.getScene().getWindow();
-        appStage.setScene(scene);
-        appStage.show();
+            Stage appStage = (Stage) delete_order.getScene().getWindow();
+            appStage.setScene(scene);
+            appStage.show();
+        }
+
     }
 
     @FXML
     void Ok2(ActionEvent event) throws IOException {
-        System.out.println("HI");
-        Parent blah = FXMLLoader.load(getClass().getResource("Adminfutureo.fxml"));
-        Scene scene = new Scene(blah);
 
-        Stage appStage = (Stage) lsview.getScene().getWindow();
-        appStage.setScene(scene);
-        appStage.show();
+        List<List<String>> a;
+
+        {
+            System.out.println("HI");
+            Parent blah = FXMLLoader.load(getClass().getResource("AdminHisOrder.fxml"));
+            Scene scene = new Scene(blah);
+
+            Stage appStage = (Stage) delete_order.getScene().getWindow();
+            appStage.setScene(scene);
+            appStage.show();
+
+
+        }
+
+
+    }
+    @FXML
+    void Ok3(ActionEvent event) throws IOException{
+
+        {
+            System.out.println("HI");
+            Parent blah = FXMLLoader.load(getClass().getResource("EditMeal.fxml"));
+            Scene scene = new Scene(blah);
+
+            Stage appStage = (Stage) delete_order.getScene().getWindow();
+            appStage.setScene(scene);
+            appStage.show();
+        }
 
     }
 
-    @FXML
-    void Ok3(ActionEvent event) throws IOException {
-        System.out.println("HI");
-        Parent blah = FXMLLoader.load(getClass().getResource("AdminEdit.fxml"));
-        Scene scene = new Scene(blah);
 
-        Stage appStage = (Stage) lsview.getScene().getWindow();
-        appStage.setScene(scene);
-        appStage.show();
+    private static List<List<String>> read_StudentOrder() {
+
+        List<List<String>> records = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(Configs.base + "StudentOrder.csv"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(COMMA_DELIMITER);
+                records.add(Arrays.asList(values));
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(records.toString());
+        return records;
+    }
+
+    private static List<List<String>> read_SOI() {
+
+        List<List<String>> records = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(Configs.base + "StudentOrderItems.csv"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(COMMA_DELIMITER);
+                records.add(Arrays.asList(values));
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(records.toString());
+        return records;
+    }
+
+    private static List<List<String>> read_SOP() {
+
+        List<List<String>> records = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(Configs.base + "StudentOrderPrice.csv"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(COMMA_DELIMITER);
+                records.add(Arrays.asList(values));
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(records.toString());
+        return records;
+    }
+
+
+
+
+
+
+
+
+    private static int readcommu_i() {
+
+        String aggregated = "";
+        try (BufferedReader br = new BufferedReader(new FileReader(Configs.base + "orderview_commun_i.csv"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                aggregated += line;
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(aggregated);
+        int a=Integer.parseInt(aggregated);
+        return a;
 
     }
 
-    @FXML
-    void Ok4(ActionEvent event) throws IOException {
-        System.out.println("HI");
-        Parent blah = FXMLLoader.load(getClass().getResource("Adminpasto.fxml"));
-        Scene scene = new Scene(blah);
 
-        Stage appStage = (Stage) lsview.getScene().getWindow();
-        appStage.setScene(scene);
-        appStage.show();
+    private List<String> read_specificOrder(int b) {
+        List<List<String>> records = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(Configs.base + "StudentOrder.csv"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(COMMA_DELIMITER);
+                records.add(Arrays.asList(values));
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(records.toString());
+
+
+        List<String> specificorder= records.get(b);
+        return specificorder;
+    }
+
+
+    private List<String> read_specificOrderItems(int b) {
+        List<List<String>> records = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(Configs.base + "StudentOrderItems.csv"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(COMMA_DELIMITER);
+                records.add(Arrays.asList(values));
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(records.toString());
+
+
+        List<String> specificorder= records.get(b);
+        return specificorder;
+    }
+
+
+    private List<String> read_specificOrderPrice(int b) {
+        List<List<String>> records = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(Configs.base + "StudentOrderPrice.csv"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(COMMA_DELIMITER);
+                records.add(Arrays.asList(values));
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(records.toString());
+
+
+        List<String> specificorder= records.get(b);
+        return specificorder;
+    }
+
+
+
+
+
+
+    protected List<List<String>> Deleting(List<List<String>> a) {
+        int c = readcommu_i();
+        List<List<String>> records2 = new ArrayList<>();
+        for (int x = 0; x < a.size(); x++) {
+            if (x == c) {
+                continue;
+            }
+            records2.add(a.get(x));
+        }
+        System.out.println(records2.toString());
+        return records2;
+    }
+
+    private static void write_StudentOrder(List<List<String>> data) {
+        String ans = "";
+        for(int i = 0; i < data.size(); i++) {
+            for(int j = 0; j < data.get(i).size(); j++) {
+                ans += String.valueOf(data.get(i).get(j));
+                if(j != data.get(i).size() - 1) ans += ",";
+            }
+            if(i != data.size() - 1) ans += "\n";
+        }
+        System.out.print(ans);
+
+        try {
+            FileWriter myWriter = new FileWriter(Configs.base + "StudentOrder.csv");
+            myWriter.write(ans);
+            myWriter.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private static void write_SOI(List<List<String>> data) {
+        String ans = "";
+        for(int i = 0; i < data.size(); i++) {
+            for(int j = 0; j < data.get(i).size(); j++) {
+                ans += String.valueOf(data.get(i).get(j));
+                if(j != data.get(i).size() - 1) ans += ",";
+            }
+            if(i != data.size() - 1) ans += "\n";
+        }
+        System.out.print(ans);
+
+        try {
+            FileWriter myWriter = new FileWriter(Configs.base + "StudentOrderItems.csv");
+            myWriter.write(ans);
+            myWriter.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private static void write_SOP(List<List<String>> data) {
+        String ans = "";
+        for(int i = 0; i < data.size(); i++) {
+            for(int j = 0; j < data.get(i).size(); j++) {
+                ans += String.valueOf(data.get(i).get(j));
+                if(j != data.get(i).size() - 1) ans += ",";
+            }
+            if(i != data.size() - 1) ans += "\n";
+        }
+        System.out.print(ans);
+
+        try {
+            FileWriter myWriter = new FileWriter(Configs.base + "StudentOrderPrice.csv");
+            myWriter.write(ans);
+            myWriter.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
 
     }
 
